2f4127d29f0f9a5d5d1b5b6244ba08015e4b79ea
Favourfisayo <Favourfisayo5@gmail.com>
2025-12-02 04:05:29 +0100

    rewrites removed

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -17,18 +17,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/:path*`,
-      },
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
